91b805452ee7ffeaacfc70b2a84ed194a97c2d26
kyleblocksom <73256380+kyleblocksom@users.noreply.github.com>
2024-03-21 13:03:16 -0700

    Update delete-customer-resources.sh

--- agents/insurance-claim-lifecycle-automation/shell/delete-customer-resources.sh
+++ agents/insurance-claim-lifecycle-automation/shell/delete-customer-resources.sh
@@ -5,14 +5,14 @@
 
 echo "Emptying and Deleting S3 Bucket: $ARTIFACT_BUCKET_NAME"
 
-aws s3 rm s3://${ARTIFACT_BUCKET_NAME} --recursive
-aws s3 rb s3://${ARTIFACT_BUCKET_NAME}
+aws s3 rm s3://${ARTIFACT_BUCKET_NAME} --region ${AWS_REGION} --recursive
+aws s3 rb s3://${ARTIFACT_BUCKET_NAME} --region ${AWS_REGION}
 
 echo "Deleting CloudFormation Stack: $STACK_NAME"
 
-aws cloudformation delete-stack --stack-name $STACK_NAME
+aws cloudformation delete-stack --stack-name $STACK_NAME --region ${AWS_REGION} 
 
-aws cloudformation describe-stacks --stack-name $STACK_NAME --query "Stacks[0].StackStatus"
-aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
+aws cloudformation describe-stacks --stack-name $STACK_NAME --region ${AWS_REGION} --query "Stacks[0].StackStatus"
+aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME --region ${AWS_REGION} 
 
 echo "DELETE_COMPLETE"
