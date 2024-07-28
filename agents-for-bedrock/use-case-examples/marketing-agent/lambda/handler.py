60abace893f247678410515394d0b2b367b49740
Paul Lu <paulcoffee@gmail.com>
2024-07-28 22:06:12 +0800

    First working version

--- agents-for-bedrock/use-case-examples/marketing-agent/lambda/handler.py
+++ agents-for-bedrock/use-case-examples/marketing-agent/lambda/handler.py
@@ -1,22 +1,15 @@
 import json
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
+from boto3.dynamodb.conditions import Key
 import os
 import base64
-import io
-from io import StringIO
 import pandas as pd
 
-# bucket for personalize output
-bucket=os.environ['BUCKET_PERSONALIZE_DATA']
-# bucket="marketing-agent-1qaz"
-# key for personalize ouput
-# key="data/output/item.json.out"
-key=os.environ['KEY_PERSONALIZE_DATA']
-# bucket for image
-# image_bucket="marketing-agent-1qaz"
-image_bucket=os.environ['BUCKET_IMAGE']
-image_prefix=os.environ['PREFIX_IMAGE']
+BUCKET_PERSONALIZE_DATA=os.environ['BUCKET_PERSONALIZE_DATA']
+KEY_PERSONALIZE_DATA=os.environ['KEY_PERSONALIZE_DATA']
+BUCKET_IMAGE=os.environ['BUCKET_IMAGE']
+ITEM_TABLE_NAME = os.environ['ITEM_TABLE_NAME']
+USER_TABLE_NAME = os.environ['USER_TABLE_NAME']
 
 def lambda_handler(event, context):
     print("Event Received:", event)
@@ -26,18 +19,21 @@ def lambda_handler(event, context):
     
     try:
         if api_path == '/getUserInfo':
-            item_id = get_parameter(event, 'item_id')
-            result = get_user_info(item_id)
+            result = get_user_info(
+                item_id=_get_parameter( event, 'item_id')
+            )
         elif api_path == '/getItemInfo':
-            item_id = get_parameter(event, 'item_id')
-            result = get_item_info(item_id)
+            result = get_item_info(
+                item_id=_get_parameter( event, 'item_id')
+            )
         elif api_path == '/contentGenerate':
-            item = get_parameter(event, 'item')
-            user = get_parameter(event, 'user')
-            campaign = get_parameter(event, 'campaign')
-            sample = get_parameter(event, 'sample')
-            image = get_parameter(event, 'image')
-            result = content_generate(item, user, campaign, sample, image)
+            result = content_generate(
+                item=_get_parameter( event, 'item'),
+                user=_get_parameter( event, 'user'),
+                campaign=_get_parameter( event, 'campaign'),
+                sample=_get_parameter( event, 'sample'),
+                image=_get_parameter( event, 'image')
+            )
         else:
             response_code = 404
             result = "Unrecognized API path: {}".format(api_path)
@@ -56,7 +52,8 @@ def lambda_handler(event, context):
         }
     }
 
-def get_parameter(event, name):
+def _get_parameter(event, name):
+    """Get a parameter from the event, or return None if not found"""
     for param in event['parameters']:
         if param['name'] == name:
             return param['value']
@@ -64,7 +61,7 @@ def get_parameter(event, name):
 
 def get_item_info(item_id):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('item_table')
+    table = dynamodb.Table(ITEM_TABLE_NAME)
     
     response = table.query(KeyConditionExpression=Key('ITEM_ID').eq(item_id))
     item = response['Items'][0]
@@ -80,7 +77,7 @@ def get_item_info(item_id):
 
 def get_user_info(item_id):
     s3 = boto3.resource('s3')
-    obj = s3.Object(bucket, key)
+    obj = s3.Object(BUCKET_PERSONALIZE_DATA, KEY_PERSONALIZE_DATA)
     body = obj.get()['Body'].read().decode('utf-8')
     data = [json.loads(line) for line in body.splitlines()]
     dataframe = pd.json_normalize(data)
@@ -88,33 +85,54 @@ def get_user_info(item_id):
     user_ids = filtered_df['output.usersList'].tolist()[0]
     
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('user_table')
+    table = dynamodb.Table(USER_TABLE_NAME)
     users = []
     for user_id in user_ids:
-        response = table.query(KeyConditionExpression=Key('USER_ID').eq(int(user_id)))
+        response = table.query(
+            KeyConditionExpression=Key('USER_ID').eq(int(user_id))
+        )
         user = response['Items'][0]
-        users.append({"user_id": str(user['USER_ID']), "age": str(user['AGE']), "gender": str(user['GENDER'])})
+        users.append(
+            {"user_id": str(user['USER_ID']), 
+             "age": str(user['AGE']), 
+             "gender": str(user['GENDER'])}
+        )
     
-    return json.dumps(call_bedrock(json.dumps("Summarize user persona for age and agenda: "+json.dumps(users))))
+    return json.dumps(
+        call_bedrock(
+            prompt=json.dumps(
+                f"Summarize user persona for age and agenda: {users}"
+            )
+        )
+    )
 
-def content_generate(item, user, campaign, sample, image):
-    image_data = download_and_decode_image(image_bucket, image)
-    prompt = f"This is a {item} photo, please write a recommendation context about {campaign} campaign and follow the rules below \
+def content_generate(item, user, campaign, sample, image_key):
+    image_data = download_and_decode_image(BUCKET_IMAGE, image_key)
+    prompt = f"This is a {item} photo, please write a recommendation context \
+        about {campaign} campaign and follow the rules below \
         <rules> \
-        1. Provide Primary text, Call to action, Headline, Description in new paragraphs with title. \
-        2. Arrange them in the given sequence: Primary text, Call to action, Headline, Description\
-        3. Start with an attention-grabbing subject line or opening sentence. This is crucial in marketing context where you need to capture interest quickly. \
-        4. Clear Call-to-Action (CTA) offer a free trial, a discount, or an invitation to learn more. \
-        5. Address Pain Points for general audience and demonstrate how your product or service provides a solution. \
-        6. Highlight key features of the product or service that are most relevant to the target audience. \
+        1. Provide Primary text, Call to action, \
+            Headline, Description in new paragraphs with title. \
+        2. Arrange them in the given sequence: Primary text, \
+            Call to action, Headline, Description\
+        3. Start with an attention-grabbing subject line or opening sentence. \
+            This is crucial in marketing context \
+            where you need to capture interest quickly. \
+        4. Clear Call-to-Action (CTA) offer a free trial, a discount, \
+            or an invitation to learn more. \
+        5. Address Pain Points for general audience and demonstrate \
+            how your product or service provides a solution. \
+        6. Highlight key features of the product or service that \
+            are most relevant to the target audience. \
         7. Include Social Testimonials by showing real-life benefits. \
-        8. Keep it Concise and avoid overly complex language or lengthy paragraphs. \
-        9. Consider your target audience including age, gender summarize in {user}. \
+        8. Keep it Concise and avoid overly \
+            complex language or lengthy paragraphs. \
+        9. Consider your target audience including age, \
+            gender summarize in {user}. \
         10. Reference historical context sample: {sample}.\
         <\rules> "
     return call_bedrock(prompt, image_data)
     
-
 def download_and_decode_image(bucket, key):
     s3 = boto3.client('s3')
     s3_response = s3.get_object(Bucket=bucket, Key=key)
@@ -124,7 +142,7 @@ def download_and_decode_image(bucket, key):
 def call_bedrock(prompt, image_data=None):
     model_id = 'anthropic.claude-3-haiku-20240307-v1:0'
     max_tokens = 1000
-    if image_data == None:
+    if image_data is None:
         messages = [{
             "role": "user",
              "content": [
@@ -152,7 +170,9 @@ def call_bedrock(prompt, image_data=None):
                 }
             ]
         }]
-    bedrock_runtime = boto3.client(service_name='bedrock-runtime', region_name='us-east-1')
+    bedrock_runtime = boto3.client(
+        service_name='bedrock-runtime', region_name='us-east-1'
+    )
     body = json.dumps(
         {
             "anthropic_version": "bedrock-2023-05-31",
--- agents-for-bedrock/use-case-examples/marketing-agent/marketing_agent/marketing_agent_stack.py
+++ agents-for-bedrock/use-case-examples/marketing-agent/marketing_agent/marketing_agent_stack.py
@@ -24,14 +24,6 @@ from constructs import Construct
 
 # import json
 
-AGENT_NAME = "marketing-bedrock-agent"
-KB_NAME = "marketing-agent-bedrock-kb"
-ACTION_GROUP_NAME = "marketing-agent-action-group"
-DATA_SOURCE_NAME = "marketing-agent-data-source"
-COLLECTION_NAME = "marketing-agent-collection"
-INDEX_NAME = "marketing-agent-index"
-NETWORK_POLICY_NAME = "marketing-agent-network-policy"
-ACCESS_POLICY_NAME = "marketing-agent-access-policy"
 AGENT_FOUNDATION_MODEL_NAME = "anthropic.claude-3-haiku-20240307-v1:0"
 AGENT_INSTRUCTION = "You are a professional marketing expert of social media marketing \
 experience and facebook post writing experience. \
@@ -41,22 +33,13 @@ experience and facebook post writing experience. \
 3. Get the target audience for the merchandise in detail\
 4. Assist user to generate the content for personalized marketing"
 
-VECTOR_FIELD_NAME = "marketing-agent-bedrock-knowledge-base-vector"
-VECTOR_INDEX_NAME = "marketing-agent-bedrock-knowledge-base-index"
-TEXT_FIELD = "AMAZON_BEDROCK_TEXT_CHUNK"
-METADATA_FIELD = "AMAZON_BEDROCK_METADATA"
-
-# ITEM_TABLE_NAME = 'item-table'
-# USER_TABLE_NAME = 'user-table'
-
-
 class MarketingAgentStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Create S3 Bucket for related data
-        data_bucket = s3.Bucket(
+        _data_bucket = s3.Bucket(
             self, "data-bucket", 
             enforce_ssl=True,
             removal_policy=RemovalPolicy.DESTROY,
@@ -64,13 +47,13 @@ class MarketingAgentStack(Stack):
         )
 
         # Upload Related Data to S3 Bucket
-        s3_deployment.BucketDeployment(self, "deploy-data",
+        _deploy_data = s3_deployment.BucketDeployment(self, "deploy-data",
             sources=[s3_deployment.Source.asset("data/")],
-            destination_bucket=data_bucket
+            destination_bucket=_data_bucket
         )
 
         # Create DynamoDB table for item data
-        item_table = dynamodb.Table(
+        _item_table = dynamodb.Table(
             self, "item-table",
             partition_key=dynamodb.Attribute(
                 name="ITEM_ID",
@@ -78,15 +61,17 @@ class MarketingAgentStack(Stack):
             ),
             import_source=dynamodb.ImportSourceSpecification(
                 input_format=dynamodb.InputFormat.csv(),
-                bucket=data_bucket,
+                bucket=_data_bucket,
                 key_prefix="DynamoDB/items.csv"
             ),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
             removal_policy=RemovalPolicy.DESTROY
         )
+        _item_table.node.add_dependency(_deploy_data)
+
         
         # Create DynamoDB table for user data
-        user_table = dynamodb.Table(
+        _user_table = dynamodb.Table(
             self, "user-table",
             partition_key=dynamodb.Attribute(
                 name="USER_ID",
@@ -94,75 +79,33 @@ class MarketingAgentStack(Stack):
             ),
             import_source=dynamodb.ImportSourceSpecification(
                 input_format=dynamodb.InputFormat.csv(),
-                bucket=data_bucket,
+                bucket=_data_bucket,
                 key_prefix="DynamoDB/users.csv"
             ),
             billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
             removal_policy=RemovalPolicy.DESTROY
         )
+        _user_table.node.add_dependency(_deploy_data)
 
-        marketing_agent_vector = opensearchserverless.VectorCollection(
-            self, "marketing-agent-vector",
-            collection_name='marketing-agent'
-        )
-
-        # marketing_agent_vector_index = opensearch_vectorindex.VectorIndex(
-        #     self, "marketing-agent-vector-index",
-        #     vector_dimensions= 1536,
-        #     collection=marketing_agent_vector,
-        #     index_name=VECTOR_INDEX_NAME,
-        #     vector_field=VECTOR_FIELD_NAME,
-        #     mappings= [
-        #         opensearch_vectorindex.MetadataManagementFieldProps(
-        #             mapping_field='AMAZON_BEDROCK_TEXT_CHUNK',
-        #             data_type='text',
-        #             filterable=True
-        #         ),
-        #         opensearch_vectorindex.MetadataManagementFieldProps(
-        #             mapping_field='AMAZON_BEDROCK_METADATA',
-        #             data_type='text',
-        #             filterable=False
-        #         )
-        #     ],
-        #     analyzer=opensearch_vectorindex.Analyzer(
-        #         character_filters=[opensearchserverless.CharacterFilterType.ICU_NORMALIZER],
-        #         tokenizer=opensearchserverless.TokenizerType.KUROMOJI_TOKENIZER,
-        #         token_filters=[
-        #             opensearchserverless.TokenFilterType.KUROMOJI_BASEFORM,
-        #             opensearchserverless.TokenFilterType.JA_STOP,
-        #         ],
-        #     )
-        # )
-
-        marketing_agent_kb = bedrock.KnowledgeBase(self, 'market-agent-kb', 
+        _marketing_agent_kb = bedrock.KnowledgeBase(self, 'market-agent-kb', 
             embeddings_model= bedrock.BedrockFoundationModel.TITAN_EMBED_TEXT_V2_1024,
-            vector_store=marketing_agent_vector, 
             instruction='UNDEFINED',    
         )
 
-        bedrock.S3DataSource(self, 'market-context-DataSource',
-            bucket= data_bucket,
-            knowledge_base=marketing_agent_kb,
+        _kb_s3_datasource = bedrock.S3DataSource(self, 'market-context-DataSource',
+            bucket= _data_bucket,
+            knowledge_base=_marketing_agent_kb,
             data_source_name='marketing-context',
             inclusion_prefixes=["context/"]
         )
-
-        marketing_agent = bedrock.Agent(
-            self,
-            "marketing-agent",
-            foundation_model=bedrock.BedrockFoundationModel.ANTHROPIC_CLAUDE_HAIKU_V1_0,
-            instruction=AGENT_INSTRUCTION,
-            should_prepare_agent=True,
-            enable_user_input=True,
-        )
-
-        marketing_agent.add_knowledge_base(marketing_agent_kb)
+        _kb_s3_datasource.node.add_dependency(_deploy_data)
 
         # Lambda Agent IAM role
         # TODO least privage
         bedrock_agent_lambda_role = iam.Role(self, "BerockAgentLambdaRole",
             assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
-            managed_policies=[iam.ManagedPolicy.from_aws_managed_policy_name("AdministratorAccess")]
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AdministratorAccess")]
         )
 
         # Pandas Lambda layer
@@ -172,7 +115,6 @@ class MarketingAgentStack(Stack):
             layer_version_arn="arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python312:9"
         )
 
-
         # Lambda function for Agent Group
         agent_function = lambda_.Function(self, "market-agent-function",
             runtime=lambda_.Runtime.PYTHON_3_12,
@@ -181,26 +123,31 @@ class MarketingAgentStack(Stack):
             layers=[pandas_layer_arn],
             timeout=Duration.seconds(600),
             environment={
-                'BUCKET_PERSONALIZE_DATA': data_bucket.bucket_name,
+                'BUCKET_PERSONALIZE_DATA': _data_bucket.bucket_name,
                 'KEY_PERSONALIZE_DATA': 'personalize/item.json.out',
-                'BUCKET_IMAGE': data_bucket.bucket_name,
-                'PREFIX_IMAGE': 'image/',
-                'ITEM_TABLE': item_table.table_name,
-                'USER_TABLE': user_table.table_name,
+                'BUCKET_IMAGE': _data_bucket.bucket_name,
+                'ITEM_TABLE_NAME': _item_table.table_name,
+                'USER_TABLE_NAME': _user_table.table_name,
             },
             role=bedrock_agent_lambda_role
         )
-
-        marketing_agent_action_group = bedrock.AgentActionGroup(self,
-            "marketing-agent-action-group",
-            action_group_name="marketing-agent-action-group",
-            action_group_executor= bedrock.ActionGroupExecutor(
-                lambda_=agent_function
-            ),
-            action_group_state="ENABLED",
-            api_schema=bedrock.ApiSchema.from_asset(
-                "data/agent-schema/openapi.json")
+        _action_group = bedrock.AgentActionGroup(self,
+                    "marketing-agent-action-group",
+                    action_group_name="marketing-agent-action-group",
+                    action_group_executor= bedrock.ActionGroupExecutor(
+                        lambda_=agent_function
+                    ),
+                    action_group_state="ENABLED",
+                    api_schema=bedrock.ApiSchema.from_asset(
+                        "data/agent-schema/openapi.json")
         )
-
-        marketing_agent.add_action_group(marketing_agent_action_group)
-
+        _marketing_agent = bedrock.Agent(
+            self,
+            "marketing-agent",
+            foundation_model=bedrock.BedrockFoundationModel.ANTHROPIC_CLAUDE_HAIKU_V1_0,
+            instruction=AGENT_INSTRUCTION,
+            should_prepare_agent=True,
+            enable_user_input=True,
+            knowledge_bases=[_marketing_agent_kb],
+        )
+        _marketing_agent.add_action_group(_action_group)
