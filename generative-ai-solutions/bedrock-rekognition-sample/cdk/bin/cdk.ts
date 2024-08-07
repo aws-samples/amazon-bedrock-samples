ff1354f69f64607f46ef59ca679105e2270e27d7
rsgrewal-aws <rsgrewal@amazon.com>
2024-08-06 19:19:46 -0700

    added initial comit for V2

--- generative-ai-solutions/bedrock-rekognition-sample/cdk/bin/cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { MultiModal } from '../lib/multi-modal-stack';
-
-const app = new cdk.App();
-const stack = new MultiModal(app, 'MultiModalLLMStack');
