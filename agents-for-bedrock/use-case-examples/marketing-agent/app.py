491989c7e3e1480354f960360e6b44c42913ce8f
Lu,Wei-Chih <weichil@amazon.com>
2024-09-18 13:38:51 +0800

    Update app.py
    Remove fake account number

--- agents-for-bedrock/use-case-examples/marketing-agent/app.py
+++ agents-for-bedrock/use-case-examples/marketing-agent/app.py
@@ -23,7 +23,7 @@ MarketingAgentStack(app, "MarketingAgentStack",
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    #env=cdk.Environment(account='XXXXX', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
     )
