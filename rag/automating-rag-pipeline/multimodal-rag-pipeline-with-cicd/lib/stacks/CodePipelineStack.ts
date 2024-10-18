ce3e632c5236bacf7d8a272f5a713530a44fa6d0
Manoj S <manojs@Manojs-MacBook-Pro.local>
2024-10-18 14:57:57 -0700

    debug: print path roots in the build process

--- rag/automating-rag-pipeline/multimodal-rag-pipeline-with-cicd/lib/stacks/CodePipelineStack.ts
+++ rag/automating-rag-pipeline/multimodal-rag-pipeline-with-cicd/lib/stacks/CodePipelineStack.ts
@@ -34,19 +34,19 @@ export class CodePipelineStack extends Stack {
       }),
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub(
-          // "manoj-selvakumar5/multimodal-rag-cicd-automation",
           "manoj-selvakumar5/amazon-bedrock-samples",
           "rag-cicd",
         ),
         commands: [
           "echo 'Current working directory:' $(pwd)",
-          "cd amazon-bedrock-samples/rag/automating-rag-pipeline/multimodal-rag-pipeline-with-cicd",
+          "cd rag/automating-rag-pipeline/multimodal-rag-pipeline-with-cicd",
           "echo 'New working directory:' $(pwd)",
           "ls -ltr",
-          "npm ci",   
-          "npm run build", 
-          "npx cdk synth"],
-        primaryOutputDirectory: 'amazon-bedrock-samples/rag/automating-rag-pipeline/multimodal-rag-pipeline-with-cicd'  // Since the root directory of the project is a sub-folder in the repo
+          "npm ci",
+          "npm run build",
+          "npx cdk synth"
+        ],
+        primaryOutputDirectory: 'rag/automating-rag-pipeline/multimodal-rag-pipeline-with-cicd'  // Updated to reflect the correct project root directory
       }),
       dockerEnabledForSynth: true,
     });
