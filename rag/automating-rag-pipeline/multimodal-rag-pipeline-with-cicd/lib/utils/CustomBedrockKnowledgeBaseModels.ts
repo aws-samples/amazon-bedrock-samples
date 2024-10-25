cbf99509faa061beee67bd299b24d656f4aa9cb3
Manoj S <manojs@Manojs-MacBook-Pro.local>
2024-10-25 11:52:13 -0700

    chore: delete multimodal folder

--- rag/automating-rag-pipeline/multimodal-rag-pipeline-with-cicd/lib/utils/CustomBedrockKnowledgeBaseModels.ts
+++
@@ -1,9 +0,0 @@
-import { BedrockKnowledgeBaseModels } from '@aws/agents-for-amazon-bedrock-blueprints';
-
-export class CustomBedrockKnowledgeBaseModels extends BedrockKnowledgeBaseModels {
-    public static readonly TITAN_EMBED_TEXT_V2 = new CustomBedrockKnowledgeBaseModels("amazon.titan-embed-text-v2:0", 512);
-
-    constructor(modelName: string, vectorDimension: number) {
-        super(modelName, vectorDimension);
-    }
-}
