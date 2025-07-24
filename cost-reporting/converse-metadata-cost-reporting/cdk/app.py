bedb259f75b4d2924b3f322f4c4003b491cdd9b7
Srikanth Reddy <srikanrk@amazon.com>
2025-07-24 00:46:40 -0500

    security validations

--- cost-reporting/converse-metadata-cost-reporting/cdk/app.py
+++ cost-reporting/converse-metadata-cost-reporting/cdk/app.py
@@ -1,12 +1,48 @@
 #!/usr/bin/env python3
 
 import aws_cdk as cdk
-
 from cdk.cdk_stack import CdkStack
+# Uncomment cdk-nag for testing
+from cdk_nag import AwsSolutionsChecks, NagSuppressions
+from aws_cdk import Aspects
 
 app = cdk.App()
-CdkStack(app, "CdkStack",
-        stack_name="BedrockCostReportingStack",
-    )
+
+# Enable CDK-nag checks
+Aspects.of(app).add(AwsSolutionsChecks(verbose=True))
+
+# Create the stack
+stack = CdkStack(
+    app,
+    "CdkStack",
+    stack_name="BedrockCostReportingStack",
+)
+
+# Enable suppressions
+NagSuppressions.add_stack_suppressions(
+    stack,
+    [
+        {
+            "id": "AwsSolutions-IAM5",
+            "reason": "Wildcard permission to allow access to all objects in the transformed bucket"
+        },
+        {
+            "id": "AwsSolutions-IAM4",
+            "reason": "Using AWS managed policies is acceptable for this solution"
+        },
+        {
+            "id": "AwsSolutions-GL1",
+            "reason": "CloudWatch Log encryption is not required for this demo solution"
+        },
+        {
+            "id": "AwsSolutions-GL3",
+            "reason": "Job bookmark encryption is not required for this demo solution"
+        },
+        {
+            "id": "AwsSolutions-S1",
+            "reason": "Transformed logs bucket does not have any sensitive data"
+        }
+    ]
+)
 
 app.synth()
